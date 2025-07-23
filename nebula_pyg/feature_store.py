1aa02f1547a864bfb5faf6ca684a74326d63b595
caifengze <fengzcw@gmail.com>
2025-07-23 18:23:32 +0800

    fix: Tag adjustment after introducing snapshot for vid

--- nebula_pyg/feature_store.py
+++ nebula_pyg/feature_store.py
@@ -9,54 +9,93 @@ from nebula_pyg.type_helper import get_feature_dim
 from nebula3.data.DataObject import ValueWrapper
 
 class NebulaFeatureStore(FeatureStore, ABC):
-    def __init__(self, gcilent, sclient, space):
+    def __init__(self, gcilent, sclient, space, idx_to_vid, vid_to_idx):
         super().__init__()
         self.gcilent = gcilent
         self.sclient = sclient
         self.space = space
+        self.idx_to_vid = idx_to_vid
+        self.vid_to_idx = vid_to_idx
+        
 
     def get_tensor(self, attr: TensorAttr, index=None, **kwargs):
         return self._get_tensor(attr, index=index, **kwargs)
 
+    # TODO: COO edge index
+    # def _get_tensor(self, attr: TensorAttr, index=None, **kwargs):
+    #     tag = attr.group_name
+    #     prop = attr.attr_name
+    #     values = []
+
+    #     # TODO: Optimize index sampling from the underlying logic instead of filtering here
+    #     N = len(self.vid_to_idx)
+    #     if index is not None:
+    #         if hasattr(index, "tolist"):
+    #             index = index.tolist()
+    #         else:
+    #             index = list(index)
+    #         for batch_vids in split_batches(index, batch_size=4096):
+    #             for part_id, batch in self.sclient.scan_vertex_async(
+    #                     self.space,
+    #                     tag,
+    #                     [prop],
+    #                     batch_size=len(batch_vids),
+    #             ):
+    #                 for node in batch.as_nodes():
+    #                     vid = node.get_id().cast()
+    #                     if vid in batch_vids:
+    #                         props = node.properties(tag)
+    #                         if prop in props:
+    #                             val = props[prop].cast()
+    #                             values.append(val)
+
+    #     else:
+    #         for part_id, batch in self.sclient.scan_vertex_async(
+    #             self.space,
+    #             tag,
+    #             [prop],
+    #             batch_size=4096,
+    #         ):
+    #             for node in batch.as_nodes():
+    #                 props = node.properties(tag)
+    #                 if prop in props:
+    #                     val = props[prop].cast()
+    #                     values.append(val)
+    #     return torch.tensor(values)
+
     def _get_tensor(self, attr: TensorAttr, index=None, **kwargs):
         tag = attr.group_name
+        print(tag)
         prop = attr.attr_name
-        values = []
 
-        # TODO: Optimize index sampling from the underlying logic instead of filtering here
-        if index is not None:
-            if hasattr(index, "tolist"):
-                index = index.tolist()
-            else:
-                index = list(index)
-            for batch_vids in split_batches(index, batch_size=4096):
-                for part_id, batch in self.sclient.scan_vertex_async(
-                        self.space,
-                        tag,
-                        [prop],
-                        batch_size=len(batch_vids),
-                ):
-                    for node in batch.as_nodes():
-                        vid = node.get_id().cast()
-                        if vid in batch_vids:
-                            props = node.properties(tag)
-                            if prop in props:
-                                val = props[prop].cast()
-                                values.append(val)
+        tag_indices = [idx for idx, (t, v) in self.idx_to_vid.items() if t == tag]
+        tag_indices = sorted(tag_indices)
 
-        else:
-            for part_id, batch in self.sclient.scan_vertex_async(
-                self.space,
-                tag,
-                [prop],
-                batch_size=4096,
-            ):
-                for node in batch.as_nodes():
+        N = len(tag_indices)
+        result = [None] * N
+        
+        idx_map = {self.idx_to_vid[idx]: i for i, idx in enumerate(tag_indices)} 
+
+        for part_id, batch in self.sclient.scan_vertex_async(
+            self.space, tag, [prop], batch_size=4096
+        ):
+            for node in batch.as_nodes():
+                vid = node.get_id().cast()
+                tag_vid = (tag, vid)
+                if tag_vid in idx_map:
+                    idx = idx_map[tag_vid]
                     props = node.properties(tag)
                     if prop in props:
                         val = props[prop].cast()
-                        values.append(val)
-        return torch.tensor(values)
+                        result[idx] = val
+
+        if index is not None:
+            out = [result[i] for i in index]
+        else:
+            out = result
+        out = [v if v is not None else 0 for v in out]
+        return torch.tensor(out)
+
 
     def _put_tensor(self, tensor, attr: TensorAttr):
         raise NotImplementedError
@@ -119,3 +158,6 @@ class NebulaFeatureStore(FeatureStore, ABC):
                 col_name = col.name.decode() if isinstance(col.name, bytes) else col.name
                 attrs.append(TensorAttr(tag, col_name))
         return attrs
+
+    def _multi_get_tensor(self, attrs: list[TensorAttr], index=None, **kwargs):
+        return [self._get_tensor(attr, index=index, **kwargs) for attr in attrs]
