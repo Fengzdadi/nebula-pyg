90bac26aab1ff8c5176e1a933fedde97b687f41f
caifengze <fengzcw@gmail.com>
2025-08-03 16:08:04 +0800

    refactor: Remove old modules that are no longer used

--- nebula_pyg/sampler.py
+++
@@ -1,9 +0,0 @@
-from torch_geometric.loader import NeighborSampler
-
-
-class NebulaNeighborSampler(NeighborSampler):
-    """
-    Reuse PyG NeighborSampler, the only difference:
-    edge_index / num_nodes provided by NebulaGraphStore.
-    """
-    pass
